85be4d8b60b9b26308f82f069cfaf203ceea8f2b
Piruin Panichphol <piruin.p@gmail.com>
2016-04-07 11:19:29 +0700

    Add marlo-sample module

--- marlo-samples/src/androidTest/java/th/or/nectec/marlo/sample/ApplicationTest.java
+++ marlo-samples/src/androidTest/java/th/or/nectec/marlo/sample/ApplicationTest.java
@@ -15,4 +15,16 @@
  * limitations under the License.
  */
 
-include ':marlo', ':samples', ':marlo-samples'
+package th.or.nectec.marlo.sample;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
